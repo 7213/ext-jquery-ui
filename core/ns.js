cee569db72bc60479d6c3ddb546a495bef5b59a6
songzheng <13720019822@163.com>
2015-11-11 22:16:15 +0800

    增加namespace冲突处理

--- core/ns.js
+++ core/ns.js
@@ -1,7 +1,14 @@
 var $ = require('jquery');
+
+var _previousEJU = $.eju;
 var eju = {
     version: '0.0.1',
-    eju: true
+    eju: true,
+    noConflict: function() {
+        return _previousEJU || this;
+    }
 };
+
+$.eju = eju;
 
 module.exports = eju;
